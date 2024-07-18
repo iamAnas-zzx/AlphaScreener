4283bbdd0a9cb6709c7f166d6fbe5d6d65d6f00a
Ahmad Anas <102130991+iamAnas-zzx@users.noreply.github.com>
2024-07-18 10:33:18 +0530

    change in user routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,24 +6,24 @@ import { ExpressError } from "../utils/ExpressError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Otp } from "../models/otp.js";
-import  twilio  from "twilio";
+import twilio from "twilio";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 
-const generateAccessAndRefereshTokens = async(userId) =>{
+const generateAccessAndRefereshTokens = async (userId) => {
   try {
-      const user = await User.findById(userId)
-      const accessToken = user.generateAccessToken()
-      const refreshToken = user.generateRefreshToken()
+    const user = await User.findById(userId)
+    const accessToken = user.generateAccessToken()
+    const refreshToken = user.generateRefreshToken()
 
-      user.refreshToken = refreshToken
-      await user.save({ validateBeforeSave: false })
+    user.refreshToken = refreshToken
+    await user.save({ validateBeforeSave: false })
 
-      return {accessToken, refreshToken}
+    return { accessToken, refreshToken }
 
 
   } catch (error) {
-      throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    throw new ApiError(500, "Something went wrong while generating referesh and access token")
   }
 }
 
@@ -45,7 +45,7 @@ router.post('/signup', async (req, res) => {
       verified: req.body.verified || false,
       isPrime: req.body.isPrime || false
     })
-   
+
     const userSaved = await user.save();
     if (!userSaved) {
       return res.status(401).send('Something went wrong')
@@ -53,13 +53,13 @@ router.post('/signup', async (req, res) => {
 
     const otp = Math.floor(100000 + Math.random() * 900000).toString(); // Generate 6-digit OTP
     //OTP taking for Otp model
-    const otpEntry = new Otp({ 
+    const otpEntry = new Otp({
       phoneNumber: userSaved.phoneNumber,
       otp: otp,
     });
     await otpEntry.save();
     console.log('otp entry saved');
-    
+
     // Send OTP via Twilio
     // await client.messages.create({
     //   body: `Your OTP is: ${otp}`,
@@ -82,13 +82,13 @@ router.post('/otpVerify', asyncHandler(async (req, res) => {
     console.log("!otpEntry")
     return res.status(400).send('Invalid OTP');
   }
-  if (otp!= otpEntry.otp) {
+  if (otp != otpEntry.otp) {
     console.log(otpEntry.otp)
     console.log(otp)
     return res.status(400).send('Invalid OTP');
   }
   await Otp.deleteOne({ phoneNumber: phoneNumber });
-  const user = await User.findOne({phoneNumber: phoneNumber});
+  const user = await User.findOne({ phoneNumber: phoneNumber });
   if (!user) {
     return res.status(400).send('Invalid OTP');
   }
@@ -98,84 +98,58 @@ router.post('/otpVerify', asyncHandler(async (req, res) => {
 }))
 
 router.post('/login', asyncHandler(async (req, res) => {
-  try{
-  const { email, password } = req.body;
-  if (!email || !password) {
-    console.log('Enter all values');
-    throw new Error('Please enter all values');
-    // console.log(Error.);  
-    // throw new ExpressError(404, 'Please enter values')
-    // return res.send('Enter all values');
-  }
-  const user = await User.findOne({ email: email })//user find
-  // if (!user) {
-  //   throw new ExpressError(404, "User does not exist")
-  // }
-  const isMatch = await user.comparePassword(password);
-  console.log(user); // Debugging: log the user instance
-  const user = await User.findOne({ email: email })
-  if (!user) {
-    throw new ExpressError(404, "User does not exist")
-  }
-  const isMatch = await user.comparePassword(password)
-  console.log(User); // Debugging: log the user instance
-  if (!isMatch) {
-    throw new Error("Invalid user credentials")
-    // return res.send('Enter all values');
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      console.log('Enter all values');
+      throw new Error('Please enter all values');
+      // console.log(Error.);  
+      // throw new ExpressError(404, 'Please enter values')
+      // return res.send('Enter all values');
+    }
+    const user = await User.findOne({ email: email })//user find
+    // if (!user) {
+    //   throw new ExpressError(404, "User does not exist")
+    // }
+    const isMatch = await user.comparePassword(password);
+    console.log(user); // Debugging: log the user instance
+    if (!user) {
+      throw new ExpressError(404, "User does not exist")
+    }
+    console.log(User); // Debugging: log the user instance
+    //at this we find the user is verified
+    //then we should generate tokens
+    // const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(user._id);
+    // console.log(accessToken);
+    // console.log(refreshToken);
+    //   const options = {
+    //     httpOnly: true,
+    //     secure: true
+    // }
 
-  }
-  //at this we find the user is verified
-  //then we should generate tokens
-  const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id);
-  console.log(accessToken);
-  console.log(refreshToken);
-//   const options = {
-//     httpOnly: true,
-//     secure: true
-// }
-// .cookie("accessToken", accessToken, options)
+    const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(user._id);
+    const options = {
+      httpOnly: true,
+      secure: true
+    }
+    // .cookie("accessToken", accessToken, options)
     // .cookie("refreshToken", refreshToken, options)
-    
 
-  return res
-    .status(200)
-    .json(
+
+    return res
+      .status(200)
+      .json(
         new ApiResponse(
-            200, 
-            {
-                accessToken, refreshToken
-            },
-            "User logged In Successfully"
+          200,
+          {
+            accessToken, refreshToken
+          },
+          "User logged In Successfully"
         )
-    )
-  }catch (err) {
+      )
+  } catch (err) {
     res.status(400).send(err.message);
   }
-    throw new ExpressError(401, "Invalid user credentials")
-  }
-
-  const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id);
-  const options = {
-    httpOnly: true,
-    secure: true
-}
-// .cookie("accessToken", accessToken, options)
-    // .cookie("refreshToken", refreshToken, options)
-    
-
-  return res
-    .status(200)
-    .json(
-        new ApiResponse(
-            200, 
-            {
-                accessToken, refreshToken
-            },
-            "User logged In Successfully"
-        )
-    )
-
-  // res.send('User Exist');
 }))
 
 export default router;
